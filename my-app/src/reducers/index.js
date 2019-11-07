78a9983c182e9bccc103aeff636bd61a30bad9d3
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-11-07 12:58:52 -0500

    added reducer file

---
+++ my-app/src/reducers/index.js
@@ -0,0 +1,29 @@
+import {FETCHING_INFO, FETCHING_FAIL} from '../actions';
+
+const initialState = {
+    //message: null,
+    isFetching: false,
+    error:''
+}
+
+export const reducer = (state = initialState, action) => {
+switch (action.type) {
+
+    case FETCHING_INFO:
+        return {
+            ...state,
+            message: action.payload,
+            isFetching: false, // <-- suppose to be true?
+            error: ''
+        };
+
+    case FETCHING_FAIL:
+        return {
+            ...state,
+            isFetching: false, // <-- suppose to be true?
+            error: ''
+        };
+        default:
+            return state;
+  }
+ };
