8ab94fe2924ab88c7dc53530a213efcf3161e36d
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-11-07 12:45:28 -0500

    added actions file

--- my-app/src/actions/index.js
+++ my-app/src/actions/index.js
@@ -1,1 +1,21 @@
 import axios from 'axios';
+
+export const FETCHING_INFO = "FETCHING_INFO"
+export const FETCHING_FAIL = "FETCHING_FAIL"
+
+export const getHalo = () => dispatch => {
+    dispatch({type: FETCHING_INFO})
+    axios
+    .get('') // <-- API
+    .then(res => {
+        console.log(res)
+        dispatch({type: FETCHING_INFO, payload: res.data})
+    })
+    .catch(err => {
+        console.log(err)
+        dispatch({
+            type: FETCHING_FAIL,
+            payload: err.response
+        });
+    });
+};
