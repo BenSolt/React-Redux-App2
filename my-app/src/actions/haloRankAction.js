8458d71af1afaede90425d58cc9184c85f9f1aa1
benjamin-solt <bensolt@gmail.com>
2021-04-15 16:22:19 -0400

    readded env file

--- my-app/src/actions/haloRankAction.js
+++ my-app/src/actions/haloRankAction.js
@@ -8,7 +8,7 @@ export const getHaloRankA = () => dispatch => {
     dispatch({type: FETCHING_INFO_HALO});
       //axios
      axiosWithAuth()
-     .get('https://www.haloapi.com/metadata/h5/metadata/csr-designations')
+     .get('metadata/h5/metadata/csr-designations')
     .then(res => {
         console.log(res.data)
         dispatch({type: FETCHING_INFO_HALO_SUCESS, payload: res.data})
